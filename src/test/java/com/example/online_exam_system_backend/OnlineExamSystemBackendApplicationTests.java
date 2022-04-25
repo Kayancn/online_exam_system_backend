32bdbd6473f8c6943d39aca708ac41c0842075eb
Kayan <1443700673@qq.com>
2022-04-25 17:21:27 +0800

    框架初始化

---
+++ src/test/java/com/example/online_exam_system_backend/OnlineExamSystemBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.online_exam_system_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineExamSystemBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
